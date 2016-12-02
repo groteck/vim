5cbfda899f5d5057c243eb35880449ab9670a282
Juan Fraire <groteck@gmail.com>
2016-12-02 12:11:39 +0100

    * Create directories if we needed

--- install.sh
+++ install.sh
@@ -2,12 +2,12 @@
 VIMRC_FILE=https://raw.githubusercontent.com/groteck/vim-config/master/vimrc
 VIM_PLUGINS_FILE=https://raw.githubusercontent.com/groteck/vim-config/master/bundles.vim
 
-mkdir .vim .vim/bundle .vim/backup .vim/swap .vim/cache .vim/undo &&
+mkdir -p .vim .vim/bundle .vim/backup .vim/swap .vim/cache .vim/undo &&
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
   https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim &&
 curl -o ~/.vim/bundles.vim  $VIM_PLUGINS_FILE &&
 curl -o ~/.vimrc $VIMRC_FILE &&
 vim -N -u ~/.vim/bundles.vim +PlugInstall +qall &&
-mkdir ~/.config &&
+mkdir -p ~/.config &&
 ln -s ~/.vim ~/.config/nvim &&
 ln -s ~/.vimrc ~/.config/nvim/init.vim
